17df69e3dd68d06d62a58cbc701a85aca4eb8777
NIhat <135535115+Nihatgunay@users.noreply.github.com>
2024-07-15 22:34:29 +0400

    Update ILoanService.cs

--- Library_Management_Application_EF/Library_Management_EF_Business/Interfaces/ILoanService.cs
+++ Library_Management_Application_EF/Library_Management_EF_Business/Interfaces/ILoanService.cs
@@ -5,7 +5,7 @@ namespace Library_Management_EF_Business.Interfaces
     public interface ILoanService
     {
         Task BorrowBook(int borrowerid, int bookid);
-        Task ReturnBook(int bookid);
+        Task ReturnBook(int bookid, DateTime returndate);
         Task<List<Borrower>> LateReturnedBorrowers();
         Task<List<Borrower>> BorrowersWithBooks();
     }
